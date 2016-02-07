026248293dcc7050d85c4b0306fb8de7880bb804
Frank McCoy <jaxfrank@comcast.net>
2016-02-07 16:32:38 -0500

    Added new IO to Robot

--- src/org/teamresistance/IO.java
+++ src/org/teamresistance/IO.java
@@ -3,6 +3,7 @@ package org.teamresistance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.VictorSP;
 
 import com.kauailabs.navx.frc.AHRS;
@@ -17,6 +18,16 @@ public class IO {
 	public static VictorSP rightDrive;
 	public static RobotDrive robotDrive;
 	
+	public static Solenoid snorflerSolenoid;
+	public static VictorSP snorflerMotor;
+	
+	public static Solenoid antlerSolenoid;
+	
+	public static Solenoid gateGrabberSolenoid;
+	public static VictorSP gateLifterMotor;
+	
+	public static Solenoid flipperSolenoid;
+	
 	public static AHRS imu;
 	
 	public static void init() {
@@ -27,6 +38,16 @@ public class IO {
 		leftDrive = new VictorSP(0);
 		rightDrive = new VictorSP(1);
 		robotDrive = new RobotDrive(leftDrive, rightDrive);
+		
+		snorflerSolenoid = new Solenoid(0);
+		snorflerMotor = new VictorSP(3);
+		
+		antlerSolenoid = new Solenoid(1);
+		
+		gateGrabberSolenoid = new Solenoid(2);
+		gateLifterMotor = new VictorSP(4);
+		
+		flipperSolenoid = new Solenoid(3);
 		
 		imu = new AHRS(SPI.Port.kMXP);
 	}
