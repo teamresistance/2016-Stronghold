b1e9d7a28d77bba56e24fca0004d655f416f4251
Rothanak So <dephyance@gmail.com>
2016-02-20 15:51:22 -0500

    Deprecate State#init, add empty defaults for State#update, State#onExit

--- src/org/teamresistance/util/state/ReturnState.java
+++ src/org/teamresistance/util/state/ReturnState.java
@@ -3,26 +3,14 @@ package org.teamresistance.util.state;
 public abstract class ReturnState extends State {
 
 	private String returnState;
-	
+
+	/* Default constructor so children don't need one */
 	public ReturnState() {
-		
 	}
 	
 	public ReturnState(String returnState) {
 		this.returnState = returnState;
 	}
-	
-	@Override
-	public abstract void init();
-
-	@Override
-	public abstract void onEntry(StateTransition e);
-
-	@Override
-	public abstract void update();
-
-	@Override
-	public abstract void onExit(StateTransition e);
 
 	public void setReturnState(String returnState) {
 		this.returnState = returnState;
