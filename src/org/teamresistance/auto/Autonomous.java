d47a0bdc555281a7883025677fd99e035719ad71
1234vipersting <josephdelcastillo@outlook.com>
2016-02-21 11:45:50 -0800

    This is Auto with (a) state machine(s) and driver station input

--- src/org/teamresistance/auto/Autonomous.java
+++ src/org/teamresistance/auto/Autonomous.java
@@ -1,6 +1,5 @@
 package org.teamresistance.auto;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.teamresistance.IO;
 
 import edu.wpi.first.wpilibj.Relay;
@@ -11,6 +10,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Autonomous extends State {
 	//private StateMachine autoMachine;
+	
+	//This defines parts that gets smartdashboard input
+	Command autonomousCommand;
+	SendableChooser position chooser
 
 	@Override
 	public void update() {
@@ -21,52 +24,20 @@ public class Autonomous extends State {
 	
 	public Autonomous() {
 		/*autoMachine = new StateMachine();		
-		int defensePosition = 2;
-		(int) SmartDashboard.getNumber("defense position");
-		int defenseType = 5;
-		(int) SmartDashboard.getNumber("defense type");
-		int goalPosition = 1;
-		(int) SmartDashboard.getNumber("goal");*/
+		*int defensePosition = 2;
+		*(int) SmartDashboard.getNumber("defense position");
+		*int defenseType = 5;
+		*(int) SmartDashboard.getNumber("defense type");
+		*int goalPosition = 1;
+		*(int) SmartDashboard.getNumber("goal");
+		*/
 		
 		//Keeping code under this for reference	
 		//autoMachine.addState(new DriveToTower(defensePosition, goalPosition, defenseType), "DriveToTower");
 		//autoMachine1.0.addState(new driveToTower(driveFoward, turnLeft, driveToTower), "driveToTower")
 				
 		
-		// These are the finish time for the tasks to finish
-		// This is in seconds
-		public void double forwardTime1 = 10;
-		public void double turnLeftTime = 15;
-		public void double forwardTime2 = 20;
 		
-		//These are motor power values
-		public void double fowardPower1 = 0.5;
-		public void double turnLeftPower = 0.25;
-		public void double fowardPower2 = 0.5;
-		public void double stopPower = 0;
-		
-		//Rotational Values
-		public void double rotateValueFoward = 0;
-		public void double rotateValueCounterClockwise = -90;
-		
-		//This sets the event of operation instead of a switch
-		//I used it because I am familiar with using this and time in conjunction
-		if (currentTime < fowardTime1){
-			//First task
-			public void arcadeDrive(fowardPower1, rotateValue);
-		} 
-		else if ( forwardTime1 <= currentTime < turnLeftTime){
-			//Second Task
-			public void arcadeDrive(turnLeftPower, rotateValueCounterClockwise);
-		} 
-		else if (turnLeftTime <= currentTime < forwardTime2){
-			//Third Task
-			public void arcadeDrive(fowardPower2, rotateValue);			
-		} 
-		else {
-			//When done stop moving
-			public void arcadeDrive(stopPower, rotateValue);	
-		}
 		
 		
 	}
